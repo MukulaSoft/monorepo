c1e4b06d4fb1754cc9486b6d6070a061422b786e
Kumina <ville.syrjala@protonmail.com>
2025-12-01 13:52:46 +0200

    feat(docs): set base path for production in Vite configuration

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
+  base: isProd ? '/monorepo/' : '/',
   plugins: [react()],
   server: {
     port: 4174,
